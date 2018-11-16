82121d0202aef8bc2728a70b91a373f5d7e93b52
40203133 <mstadnik01@qub.ac.uk>
2018-11-16 19:39:00 +0000

    user story P3 bug has been squashed

--- app/src/main/java/uk/ac/qub/eeecs/game/PerformanceScreen.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/PerformanceScreen.java
@@ -1,48 +1,52 @@
 package uk.ac.qub.eeecs.game;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Rect;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-
 import uk.ac.qub.eeecs.gage.Game;
 import uk.ac.qub.eeecs.gage.engine.AssetManager;
 import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
 import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
-import uk.ac.qub.eeecs.gage.engine.input.Input;
-import uk.ac.qub.eeecs.gage.engine.input.TouchEvent;
-import uk.ac.qub.eeecs.gage.ui.PushButton;
-import uk.ac.qub.eeecs.gage.world.GameObject;
+
 import uk.ac.qub.eeecs.gage.world.GameScreen;
-import uk.ac.qub.eeecs.gage.world.LayerViewport;
-import uk.ac.qub.eeecs.gage.world.ScreenViewport;
 
-public class PerformanceScreen extends GameScreen{
 
-    //Keep count of the number of objects created
-    private int numberOfObjectsCreated = 0;
+public class PerformanceScreen extends GameScreen {
+
+
+    //Create random object
     private Random random = new Random();
+    //Create blank assetManager
     private AssetManager assetManager;
+    //to store the bitmap of the rectangle
     private Bitmap rectangle;
 
+    //store the number of objects created
+    private int numberOfObjectsCreated = 0;
+    //total number of objects to create
+    private  int numberOfObjectsToCreate = 10;
+    //array to store rectangles
+    private Rect[] rectArray = new Rect[numberOfObjectsToCreate];
     //size of the screen
     private int width;
     private int height;
 
+    private Rect sourceRect;
 
-    private double rectangleLeft,rectangleTop;
 
-    public PerformanceScreen(Game game)
-    {super("PerformanceScreen", game);
+    private double rectangleLeft, rectangleTop,rectangleBottom,rectangleRight;
 
+    public PerformanceScreen(Game game) {
+        super("PerformanceScreen", game);
+
+        //Get asset manager to load and store the image of rectangle
         assetManager = mGame.getAssetManager();
-        assetManager.loadAndAddBitmap("rectangle","img/rectangle.jpg");
+        assetManager.loadAndAddBitmap("rectangle", "img/rectangle.jpg");
         rectangle = assetManager.getBitmap("rectangle");
-
+        //object rectangle which will be used to draw on the screen
+        sourceRect = new Rect(0, 0, rectangle.getWidth(), rectangle.getHeight());
     }
+
     @Override
     public void update(ElapsedTime elapsedTime) {
 
@@ -50,31 +54,43 @@ public class PerformanceScreen extends GameScreen{
 
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
-       graphics2D.clear(Color.WHITE);
+        graphics2D.clear(Color.WHITE);
 
-         width = graphics2D.getSurfaceWidth();
-         height = graphics2D.getSurfaceHeight();
+        //getting the width and height of the screen used
+        width = graphics2D.getSurfaceWidth();
+        height = graphics2D.getSurfaceHeight();
 
-        //draw the rectangle
-        drawRectangle(graphics2D);
+        //If the desired number of objects haven't been created call method createRectangle
+        if(numberOfObjectsCreated < numberOfObjectsToCreate)
+        {
+            createRectangle();
+        }
 
+        //Draw all of the stored rectangles
+        for(int i = 0; i < numberOfObjectsCreated; i++)
+        {
+            graphics2D.drawBitmap(rectangle,sourceRect,rectArray[i],null);
+        }
     }
 
     //Methods
 
-    public void drawRectangle(IGraphics2D graphics2D)
+    public void createRectangle()
     {
+            //Randomizing the initial position of the image rectangle
+            rectangleLeft = random.nextInt(55)+20;
+            rectangleTop = random.nextInt(55)+20;
 
-        if(numberOfObjectsCreated < 10)
-        {
-            rectangleLeft = random.nextInt(84)+1;
-            rectangleTop = random.nextInt(84)+1;
-            Rect sourceRect = new Rect(
-                    0, 0, rectangle.getWidth(), rectangle.getHeight());
+            //randomizing the height and width of the image rectangle
+            rectangleBottom = random.nextInt(12)+11;
+            rectangleRight = random.nextInt(12)+11;
             Rect destRect = new Rect(
-                    (int) (width * rectangleLeft/100), (int) (height * rectangleTop/100), (int) (width * (rectangleLeft/100+0.15)), (int) (height * (rectangleTop/100+0.15)));
-            graphics2D.drawBitmap(rectangle, sourceRect, destRect,null);
+                    (int) (width * rectangleLeft/100), (int) (height * rectangleTop/100), (int) (width * (rectangleLeft/100+rectangleRight/100)), (int) (height * (rectangleTop/100+rectangleBottom/100)));
+
+            // Storing the destRect object to an array for drawing to screen
+            rectArray[numberOfObjectsCreated] = destRect;
+            //incrementing the variable numberOfObjectsCreated as a rectangle has been created
             numberOfObjectsCreated++;
-        }
+
     }
 }

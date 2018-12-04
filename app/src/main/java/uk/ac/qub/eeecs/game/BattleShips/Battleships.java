38d40930f198b2108c84e79ed81e05721cb5313b
User1 <hcunningham02@qub.ac.uk>
2018-12-04 11:41:40 +0000

    Creating the main Battleships class - User Story 7 Sprint 3 (40201925)

---
+++ app/src/main/java/uk/ac/qub/eeecs/game/BattleShips/Battleships.java
@@ -0,0 +1,14 @@
+package uk.ac.qub.eeecs.game.BattleShips;
+//40201925 - Hannah
+public class Battleships {
+    //User Story 7: Creating the main Battleships class
+    //includes main variables such as the ships, board and number of hits
+
+    public static void main(String[] args) {
+        int[][] board = new int[10][10]; //position of the board 10x10 that will be stored
+        int[][] ship = new int[3][2]; //position of the ship stored (row & column)
+        int[] shoot = new int[2]; //num of hits
+        int attempts = 0; //attempts
+
+    }
+}

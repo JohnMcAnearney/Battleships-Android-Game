42f5dff661d5ef73bb4f4dac0d7bb6dcab3ccf30
40204308 <mjonak01@qub.ac.uk>
2018-12-23 17:55:17 +0000

    Touched up the shot randomiser

--- app/src/main/java/uk/ac/qub/eeecs/game/BattleShips/BattleshipsAI.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/BattleShips/BattleshipsAI.java
@@ -2,7 +2,7 @@ package uk.ac.qub.eeecs.game.BattleShips;
 
 import java.util.ArrayList;
 import java.util.Queue;
-
+import java.util.LinkedList;
 public class BattleshipsAI {
     //This class is used to decide for the AI where to hit next and store its shots so far so it can make a more educated shot
 
@@ -23,38 +23,51 @@ public class BattleshipsAI {
         //each game played on the hard difficulty
     private Queue<String> hardShotSequence;
 
-    //This method randomly decides which of the two patterns to populates the shot sequence queue with and does that
-    //Still to come - shuffling the queue so that each game is different
-    public void randomiseShotSequence(){
-        int x, j;
-        if (Math.random()<0.5){
-            x = 1;
-        }else {
-            x = 2;
-        }
-        for (int i = 0; i<boardLength;i+=2){
-            j = x;
-            while (j<boardLength){
-                hardShotSequence.add(String.valueOf((char)(44+i) + j));
-                if(j+1<boardLength)
-                    hardShotSequence.add(String.valueOf((char)(44+i+1) + (j +1 )));
-                j+=2;
+        //This method randomly decides which of the two patterns to populates the shot sequence queue with and does that
+            //Does this by deciding whether the first square will be A1 or A2, then proceeds to add B2 or B1 respectively
+        //Still to come - shuffling the queue so that each game is different
+
+        /*  x - determines the first squares coordinate for each loop
+            y - determines whether the corresponding square in the row below is before or after
+            j - integer used to go iterate through the rows which is reset to the value of x at the start of each loop
+            */
+        public void randomiseShotSequence(){
+            Queue<String> hardShotSequence = new LinkedList<>();
+            int x, y,j ;
+            if (Math.random()<0.5){
+                x = 1;
+                y = 1;
+            }else {
+                x = 2;
+                y = -1;
+            }
+
+
+            for (int i = 0; i<boardLength;i+=2){
+                j = x;
+                while (j<=boardLength){
+                    hardShotSequence.add(String.valueOf(String.valueOf((char)(65+i)) + j));
+                    if(j+1*y<=boardLength)
+                        hardShotSequence.add(String.valueOf(String.valueOf((char)(65+i+1)) + (j + 1*y )));
+                    j+=2;
+                }
             }
         }
+
+
+        //this method will be called by the easiest class, just randomly picks a point with no strategy
+        public String randomShot(){
+            int x,y;
+            String shot;
+            do{
+            x = (int)Math.round(Math.random()*boardLength);
+            y = (int)Math.round(Math.random()*boardLength);}while(board[y][x]!=0);
+
+            board[y][x] = 1;
+            return String.valueOf((char)(44+x) + y);
+        }
+
+
+
+
     }
-    //this method will be called by the easiest class, just randomly picks a point with no strategy
-    public String randomShot(){
-        int x,y;
-        String shot;
-        do{
-        x = (int)Math.round(Math.random()*boardLength);
-        y = (int)Math.round(Math.random()*boardLength);}while(board[y][x]!=0);
-
-        board[y][x] = 1;
-        return String.valueOf((char)(44+x) + y);
-    }
-
-
-
-
-}

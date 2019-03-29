aac4ab622257ef39317d8f3c1adbfdce1a9f580f
40203133 <mstadnik01@qub.ac.uk>
2019-03-29 15:38:51 +0000

    More tests completed

--- app/src/androidTest/java/uk/ac/qub/eeecs/gage/BoardSetUpTest.java
+++ app/src/androidTest/java/uk/ac/qub/eeecs/gage/BoardSetUpTest.java
@@ -121,12 +121,151 @@ public class BoardSetUpTest {
 
 
     @Test
-    public void calculateShipRatioX()
+    public void SetUpShipmBound()
     {
-        assertTrue(assetManager.loadAndAddBitmap("AircraftCarrier", "img/AircraftCarrier.png"));
-        int shipBitmapHeight = assetManager.getBitmap("AircraftCarrier").getWidth();
+        //load bitmaps
+        assetManager.loadAndAddBitmap("AircraftCarrier", "img/AircraftCarrier.png");
+        assetManager.loadAndAddBitmap("CargoShip", "img/CargoShip.png");
+        assetManager.loadAndAddBitmap("CruiseShip", "img/CruiseShip.png");
+        assetManager.loadAndAddBitmap("Submarine", "img/Submarine.png");
+        assetManager.loadAndAddBitmap("Destroyer", "img/Destroyer.png");
+        //Set up Ship objects
+        Ship aircraftCarrier = new Ship("AircraftCarrier", 0.5f,0.5f,assetManager.getBitmap("AircraftCarrier"), 5);
+        Ship cargoShip = new Ship("CargoShip", 0.5f,0.5f,assetManager.getBitmap("CargoShip"), 4);
+        Ship cruiseShip = new Ship("CruiseShip", 0.5f,0.5f,assetManager.getBitmap("CruiseShip"), 4);
+        Ship submarine = new Ship("Submarine", 0.5f,0.5f,assetManager.getBitmap("Submarine"), 3);
+        Ship destroyer = new Ship("Destroyer",  0.5f,0.5f,assetManager.getBitmap("Destroyer"), 2);
+        Ship[] shipArray = new Ship[]{aircraftCarrier,cargoShip,cruiseShip,destroyer,submarine};
 
+
+        int screenWidth = 1920;
+        int screenHeight = 1080;
+        float screenHeightOffset = 0.1f;
+        //Setting the ships bounding box, including the x,y co-ordinates and the half width and half height using bounding box setter
+        for (Ship ship : shipArray) {
+            screenHeightOffset = screenHeightOffset + 0.08f;
+            ship.setmBound(screenWidth * 0.015f,
+                    screenHeight * screenHeightOffset,
+                    (100 * ship.getShipLength()) / 2.0f,
+                    ((100) / 10f) / 2f); }
+
+                    assertEquals(29,Math.round(shipArray[0].getmBound().x));
+        assertEquals(194,Math.round(shipArray[0].getmBound().y));
+        assertEquals(250,Math.round(shipArray[0].getmBound().halfWidth));
+        assertEquals(5,Math.round(shipArray[0].getmBound().halfHeight));
+
+        assertEquals(29,Math.round(shipArray[1].getmBound().x));
+        assertEquals(281,Math.round(shipArray[1].getmBound().y));
+        assertEquals(200,Math.round(shipArray[1].getmBound().halfWidth));
+        assertEquals(5,Math.round(shipArray[1].getmBound().halfHeight));
+
+        assertEquals(29,Math.round(shipArray[2].getmBound().x));
+        assertEquals(367,Math.round(shipArray[2].getmBound().y));
+        assertEquals(200,Math.round(shipArray[2].getmBound().halfWidth));
+        assertEquals(5,Math.round(shipArray[2].getmBound().halfHeight));
+
+        assertEquals(29,Math.round(shipArray[3].getmBound().x));
+        assertEquals(454,Math.round(shipArray[3].getmBound().y));
+        assertEquals(100,Math.round(shipArray[3].getmBound().halfWidth));
+        assertEquals(5,Math.round(shipArray[3].getmBound().halfHeight));
+
+        assertEquals(29,Math.round(shipArray[4].getmBound().x));
+        assertEquals(540,Math.round(shipArray[4].getmBound().y));
+        assertEquals(150,Math.round(shipArray[4].getmBound().halfWidth));
+        assertEquals(5,Math.round(shipArray[4].getmBound().halfHeight));
     }
+
+    private boolean boxContainsInput(float leftX,  float rightX, float TopY, float bottomY, float inputX, float inputY)
+    {
+        if(inputX > leftX&&
+                inputX < rightX &&
+                inputY > TopY &&
+                inputY < bottomY)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Test
+    public void boxContainsInputTest()
+    {
+        assertFalse(boxContainsInput(0,0,0,0,0,0));
+        assertFalse(boxContainsInput(0,0,0,0,10,10));
+        assertFalse(boxContainsInput(100,100,100,100,0,0));
+        assertFalse(boxContainsInput(100,100,100,100,99.9f,99.9f));
+        assertFalse(boxContainsInput(1000,1200,1000,1200,1000,1000));
+        assertFalse(boxContainsInput(500,800,400,600,0,0));
+        assertFalse(boxContainsInput(200,350,200,220,250,500));
+
+        assertTrue(boxContainsInput(0,700,0,800,50,50));
+        assertTrue(boxContainsInput(0,100,0,100,99,99));
+        assertTrue(boxContainsInput(0,100,0,100,1,1));
+        assertTrue(boxContainsInput(0,200,0,300,55.5f,55.5f));
+    }
+
+    private int binarySearchRows(float[][] array,int numberOfSmallBox, int lower,int higher, float x, float y)
+    {
+        //Check if the lower bound is less than or equal to higher bound
+        //this is used to ensure when the user has not clicked onto a small box, the loop
+        //is broken
+        //if lower bound is less than the higher bound find a mid value
+        if(lower <higher) {
+            int mid = numberOfSmallBox + ((lower + higher) / 2);
+
+            //if the mid small box contains the user's input x and y values the box
+            // have been found, return the number of the small box
+            if (boxContainsInput(array[mid][0],array[mid][2], array[mid][1], array[mid][3], x, y)) {
+                return mid;
+            }
+            //if the user's input x value is lower than the current small box, recursive call
+            //current method with the higher bound set to mid
+            if (x < array[mid][0] && x < array[mid][2]){
+                return binarySearchRows(array, numberOfSmallBox, lower, mid % 10 , x, y);}
+
+            //if the user's input x value is higher than the current small box, recursive call
+            //current method with the lower bound set to mid
+            else if (x > array[mid][0] && x > array[mid][2]){
+                return binarySearchRows(array, numberOfSmallBox, mid % 10 +1 , higher, x, y);}
+
+        }
+        return -1;
+    }
+
+    @Test
+    public void binarySearchRowsTest()
+    {
+        //Set up 2D array
+        //offsets for setting co-ordinates
+        int offsetLeftX = 0;
+        int offsetRightX = 5;
+        int offsetTopY = 0;
+        int offsetBottomY = 5;
+        int numberOfBoxesStored = 0;
+        float[][] boxArray = new float[100][4];
+        //populate the 2D array store in order, left , top, right ,bottom co-ordinates
+        for(int i = 0; i < 10; i ++)
+        {
+            offsetLeftX = 0;
+            offsetRightX = 5;
+            for(int j = 0; j < 10; j++)
+            {
+                boxArray[numberOfBoxesStored][0] = offsetLeftX;
+                boxArray[numberOfBoxesStored][2] = offsetRightX;
+                boxArray[numberOfBoxesStored][3] = offsetBottomY;
+                boxArray[numberOfBoxesStored][1] = offsetTopY;
+                offsetLeftX += 5;
+                offsetRightX += 5;
+                numberOfBoxesStored++;
+            }
+            offsetTopY +=5;
+            offsetBottomY +=5;
+        }
+
+        assertEquals(0,binarySearchRows(boxArray,0,0,9,3,3));
+    }
+
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     //Tests Made by JoSh Macaroni (402696969)
